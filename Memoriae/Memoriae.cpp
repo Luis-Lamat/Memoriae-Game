768008c94d800dd3131a09166581236ccd241e8b
Luis-Lamat <beto_alaska@hotmail.com>
2016-03-16 13:30:47 -0600

    Model advances...

--- Memoriae/Memoriae.cpp
+++ Memoriae/Memoriae.cpp
@@ -7,3 +7,39 @@
 //
 
 #include "Memoriae.hpp"
+
+void Memoriae::cleanBoard() {
+    for (int i = 0; i < matrixSize; i++) {
+        for (int j = 0; j < matrixSize; j++) {
+            board[i][j] = _OFF;
+        }
+    }
+}
+
+Memoriae::Memoriae() {
+    this->cleanBoard();
+    level = 0;
+    score = 0;
+    gameState = STATE_PLAYING;
+}
+
+void Memoriae::start() {
+    this->cleanBoard();
+}
+
+void Memoriae::pause() {
+    this->gameState = STATE_PAUSED;
+}
+
+State Memoriae::getState() {
+    return this->gameState;
+}
+
+void Memoriae::selectObjectAt(int row, int col) {
+    int mSize = this->getActualSize();
+    if (row < 0 || row >= mSize || col < 0 || col >= mSize) {
+        printf("Error: matrix selection index out of bounds\n");
+        return;
+    }
+    board[row][col] = _ON;
+}
--- Memoriae/Memoriae.hpp
+++ Memoriae/Memoriae.hpp
@@ -13,7 +13,12 @@
 
 #endif /* Memoriae_hpp */
 
+/* --------------------------- Global Variables ----------------------------- */
+# define _ON 1
+# define _OFF 0
+
 const int matrixSize = 10;
+const int startingMatrixSize = 3;
 
 typedef enum {
     STATE_PLAYING,
@@ -25,15 +30,23 @@ typedef enum {
 class Memoriae {
     
 private:
+    // Attributes
     int board[matrixSize][matrixSize];
     int level;
     int score;
     State gameState;
     
+    // Helper functions
+    void cleanBoard();
+    
 public:
     Memoriae();      // Constructor
     void start();    // Uses shuffle
     void pause();    // Setter for the gameState
-    void getState(); // Getter for the gameState
+    void getLevel(); // Getter for the level
     void selectObjectAt(int row, int col); // Game mechanic
+    
+    int getActualSize() { return level + startingMatrixSize; };
+    
+    State getState();// Getter for the gameState
 };
