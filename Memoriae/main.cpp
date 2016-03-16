f4ab18eb42edf92d754270eb233785e897c260ae
Luis-Lamat <beto_alaska@hotmail.com>
2016-03-15 22:36:21 -0600

    Initial Commit

---
+++ Memoriae/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Memoriae
+//
+//  Created by Luis Alberto Lamadrid on 3/15/16.
+//  Copyright © 2016 Luis Alberto Lamadrid. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
