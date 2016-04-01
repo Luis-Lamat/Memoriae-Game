9f1fce9061444f22d74b7e00b4583cdba6d6ac1a
manolo <manolosavi@me.com>
2016-03-31 20:15:06 -0600

    Added function to draw X by Y spheres centered on screen.

--- Memoriae/main.cpp
+++ Memoriae/main.cpp
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
-
+#include <math.h>
 
 #pragma mark - Global Variables
 /* --------------------------- Global Variables ----------------------------- */
@@ -33,27 +33,53 @@ void reshape(int w, int h) {
 	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glFrustum(X_MIN, X_MAX, Y_MIN, Y_MAX, 1.0, 10.0);
+	glFrustum(X_MIN, X_MAX, Y_MIN, Y_MAX, 1, 10);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(0, 0, 1.1, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	gluLookAt(0, 0, 1.1, 0, 0, 0, 0, 1, 0);
+}
+
+void paintSpheres(int spheresPerRow, int spheresPerColumn, float maxWidth) {
+	int maxSpheres = fmax(spheresPerRow, spheresPerColumn);
+	float radius = maxWidth/maxSpheres;
+	
+	glColor3f(0.1, 0.1, 0.1);
+	
+	glPushMatrix();
+	glTranslatef(-maxWidth*((float)spheresPerRow/maxSpheres), -maxWidth*((float)spheresPerColumn/maxSpheres), 0);//center all spheres
+	glScalef(1, 1, 0.1);//scale spheres on Z so they don't distort
+	for (int i=0; i < spheresPerColumn; i++) {
+		glPushMatrix();
+		glTranslatef(radius, i*(2*radius), 0);//starting position for each row
+		
+		for (int j=0; j < spheresPerRow; j++) {
+			glPushMatrix();
+			glTranslatef(j*(2*radius), radius, 0);//starting position for each column
+			glScalef(0.8, 0.8, 0.8);//make spheres smaller so they don't touch
+			glutSolidSphere(radius, 30, 30);
+			glPopMatrix();
+		}
+		glPopMatrix();
+	}
+	glPopMatrix();
 }
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	
+	paintSpheres(4, 3, fmin(X_MAX, Y_MAX));
 	
 	glutSwapBuffers();
 }
 
 void init() {
-	glClearColor (0.2039, 0.6588, 0.3254, 1.0);
-	glColor3f(0.0, 0.0, 0.0);
+	glClearColor(0.2039, 0.6588, 0.3254, 1.0);
+	glColor3f(0, 0, 0);
 }
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
-	glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(0, 0);
 	glutCreateWindow("Memoriae");
@@ -61,4 +87,6 @@ int main(int argc, char** argv) {
 	glutDisplayFunc(display);
 	init();
 	glutMainLoop();
+	
+	return 0;
 }
